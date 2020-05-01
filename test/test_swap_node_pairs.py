b4eac664b247e81267df943d99f456402b0cbb85
Colin Rennie <colinmatthew@gmail.com>
2020-05-01 10:56:57 -0700

    changes

--- test/test_swap_node_pairs.py
+++ test/test_swap_node_pairs.py
@@ -2,9 +2,12 @@ import pytest
 from leetcode.swap_node_pairs import ListNode
 from leetcode.swap_node_pairs import Solution
 
-def test_basic():
-    test_head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, None))))
+@pytest.fixture
+def list_node():
+    return ListNode(1, ListNode(2, ListNode(3, ListNode(4, None))))
+
+def test_basic(list_node):
     expected = ListNode(2, ListNode(1, ListNode(4, ListNode(3, None))))
 
     sol = Solution()
-    assert sol.swapPairs(test_head) == expected
+    assert sol.swapPairs(list_node) == expected
